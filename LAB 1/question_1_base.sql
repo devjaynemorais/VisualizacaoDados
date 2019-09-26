aa4f318876ece0508737ffcb17330e490e146b37
Jayne Morais <morais.jayne.ti@gmail.com>
2019-09-26 09:45:10 -0300

    Correção

--- LAB 1/question_1_base.sql
+++ LAB 1/question_1_base.sql
@@ -1,11 +1,11 @@
 
-SELECT 'Computer_Science' as subarea, sub.Computer_Science as total, sub.Computer_Science_all as geral
+SELECT 'Computer Science' as subarea, sub.Computer_Science as total, sub.Computer_Science_all as geral
 FROM sigcas_ethnicity.total_subarea_new sub
 UNION ALL
-SELECT 'Data_Engineering' as subarea, sub.Data_Engineering as total, sub.Data_Engineering_all as geral
+SELECT 'Data Engineering' as subarea, sub.Data_Engineering as total, sub.Data_Engineering_all as geral
 FROM sigcas_ethnicity.total_subarea_new sub
 UNION ALL
-SELECT 'Software_Engineering' as subarea, sub.Software_Engineering as total, sub.Software_Engineering_all as geral
+SELECT 'Software Engineering' as subarea, sub.Software_Engineering as total, sub.Software_Engineering_all as geral
 FROM sigcas_ethnicity.total_subarea_new sub
 UNION ALL
 SELECT 'Theory' as subarea, sub.Theory as total, sub.Theory_all as geral
