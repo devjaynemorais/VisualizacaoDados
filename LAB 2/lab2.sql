ba9cc3c96f26e41e36f558cfadb1194f21a81d23
Jayne Morais <morais.jayne.ti@gmail.com>
2019-10-10 16:24:37 -0300

    Lab 2

---
+++ LAB 2/lab2.sql
@@ -0,0 +1,21 @@
+ 
+SELECT tab.ano, tab.genero, tab.etnia, COUNT(tab.paper_key) as total_paper
+FROM (SELECT 
+	et.name as nome, 
+	et.l1 as etnia, 
+	(CASE WHEN et.gender = '-' THEN 'N/A' ELSE et.gender END) AS genero,
+	conf.year AS ano,
+	pap.paper_key,
+	conf.cs AS Computer_Science,
+	conf.de AS Data_Engineering,
+	conf.se AS Software_Engineering,
+	conf.th AS Theory
+	FROM sigcas_ethnicity.ethnicity et
+	INNER JOIN sigcas_ethnicity.authors aut
+	INNER JOIN sigcas_ethnicity.papers pap
+	INNER JOIN sigcas_ethnicity.conferences conf
+	WHERE et.name = aut.name
+	AND aut.paper_key = pap.paper_key
+	AND pap.conf_key = conf.conf_key) tab
+    GROUP BY tab.genero, tab.ano, tab.etnia
+    ORDER BY tab.ano
